14e0c9ea26c4567f9315275b00f316a558af0b11
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-11-30 13:45:18 -0600

    _fix imports

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const fs = require("fs")
+const path = require("path")
+const { promisify } = require('util')
+
+// TODO move these helper functions into a seperate file
+
 let getFiles = async function(dir) {
     const readdir = promisify(fs.readdir)
     const stat    = promisify(fs.stat)
@@ -16,35 +22,35 @@ let absolutePath = function(relativeLocation) {
 }
 
 let set = function(obj, attributeList, value) {
-        // convert string values into lists
-        if (typeof attributeList == 'string') {
-            attributeList = attributeList.split('.')
-        }
-        if (attributeList instanceof Array) {
-            try {
-                var lastAttribute = attributeList.pop()
-                for (var elem of attributeList) {
-                    // create each parent if it doesnt exist
-                    if (!(obj[elem] instanceof Object)) {
-                        obj[elem] = {}
-                    }
-                    // change the object reference be the nested element
-                    obj = obj[elem]
-                }
-                obj[lastAttribute] = value
-            } catch (error) {
-                console.warn("the set function was unable to set the value for some reason, here is the original error message",error)
-                console.warn(`the set obj was:`,obj)
-                console.warn(`the set attributeList was:`,attributeList)
-                console.warn(`the set value was:`,value)
-            }
-        } else {
-            console.log(`obj is:`,obj)
-            console.log(`attributeList is:`,attributeList)
-            console.log(`value is:`,value)
-            console.error(`There is a 'set' function somewhere being called and its second argument isn't a string or a list (see values above)`);
-        }
+    // convert string values into lists
+    if (typeof attributeList == 'string') {
+        attributeList = attributeList.split('.')
     }
+    if (attributeList instanceof Array) {
+        try {
+            var lastAttribute = attributeList.pop()
+            for (var elem of attributeList) {
+                // create each parent if it doesnt exist
+                if (!(obj[elem] instanceof Object)) {
+                    obj[elem] = {}
+                }
+                // change the object reference be the nested element
+                obj = obj[elem]
+            }
+            obj[lastAttribute] = value
+        } catch (error) {
+            console.warn("the set function was unable to set the value for some reason, here is the original error message",error)
+            console.warn(`the set obj was:`,obj)
+            console.warn(`the set attributeList was:`,attributeList)
+            console.warn(`the set value was:`,value)
+        }
+    } else {
+        console.log(`obj is:`,obj)
+        console.log(`attributeList is:`,attributeList)
+        console.log(`value is:`,value)
+        console.error(`There is a 'set' function somewhere being called and its second argument isn't a string or a list (see values above)`);
+    }
+}
 
 let backendFunctions
 module.exports = {
